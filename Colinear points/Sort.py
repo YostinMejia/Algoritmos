2522b46b26ba0b6f19d32bc31b2afeff16fb82d3
Yostin <yostinprograming@gmail.com>
2023-08-18 18:58:03 -0500

    merge sort malo

--- Colinear points/Sort.py
+++ Colinear points/Sort.py
@@ -1,89 +1,42 @@
-# def Merge(array, auxiliar, low, mid, high):
-#     i = low
-#     j = mid+1
-#     k = low
+def Merge(array, auxiliar, low, mid, high):
+    i = low
+    j = mid+1
+    k = low
     
-#     while(i <= mid and j <=high):
-#         if array[i] <= array[j]:
-#             auxiliar[k] = array[i]
-#             i+=1
-#         else:
-#             auxiliar[k] = array[j]
-#             j+=1
-#         k+=1
-#     # Cuando i o j lleguen a su límite
-#     if (i>mid):
-#         while (j <=high):
-#             auxiliar[k] = array[j]
-#             j+=1 
-#             k+=1
-#     else:
-#         while (i <=mid):
-#             auxiliar[k] = array[i]
-#             i+=1
-#             k+=1
-#     # 
+    while(i <= mid and j <=high):
+        if array[i] <= array[j]:
+            auxiliar[k] = array[i]
+            i+=1
+        else:
+            auxiliar[k] = array[j]
+            j+=1
+        k+=1
+    # Cuando i o j lleguen a su límite
+    if (i>mid):
+        while (j <=high):
+            auxiliar[k] = array[j]
+            j+=1 
+            k+=1
+    else:
+        while (i <=mid):
+            auxiliar[k] = array[i]
+            i+=1
+            k+=1
+    # 
     
 
-# def MergeSort(array, auxiliar,low, high):
-#     if low < high:
-#         mid = (low+high)//2
-#         MergeSort(array, auxiliar, low, mid)
-#         MergeSort(array, auxiliar, mid+1, high)
-#         Merge(array, auxiliar, low, mid, high)
-                  
+def MergeSort(array, auxiliar,low, high):
+    if low < high:
+        mid = (low+high)//2
+        MergeSort(array, auxiliar, low, mid)
+        MergeSort(array, auxiliar, mid+1, high)
+        Merge(array, auxiliar, low, mid, high)
     
-# a = [3,4,5,6,1,3]    
-# aux = [3,4,5,6,1,3]    
-# # print(aux)
-# MergeSort(a,aux,0,len(a)-1)
+    
+a = [3,4,5,6,1,3]    
+aux = [3,4,5,6,1,3]    
 # print(aux)
-
-# Punto 1: • Dado dos enteros que indican la hora y los minutos muestre el ángulo
-# menor entre las dos manecillas de un reloj análogo 
-# (código hecho por Laura López y Paulina Cano)
-
-# función n impar
-def print_diagonal_zigzag_IMP(matrix):    # O(n)
-    n = len(matrix)    # O(1)
-    result = []        # O(1)
-
-    for i in range(n):    # O(n)
-        indexes = range(i, -1, -1) if i % 2 == 0 else range(0, i + 1)    # O(n)
-        result.extend(matrix[j][i - j] for j in indexes)    # O(n)
-    for i in range(1, n):    # O(n)
-        indexes = range(n - 1, i - 1, -1) if i % 2 == 0 else range(i, n)    # O(n)
-        result.extend(matrix[j][i + n - j - 1] for j in indexes)    # O(n)
-
-    print(", ".join(map(str, result)))    # O(1)
-
-# función n par
-def print_diagonal_zigzag(matrix):    # O(n)
-    n = len(matrix)    # O(1)
-    result = []    # O(1)
-
-    for i in range(n):    # O(n)
-        indexes = range(i, -1, -1) if i % 2 == 0 else range(0, i + 1)    # O(n)
-        result.extend(matrix[j][i - j] for j in indexes)    # O(n)
-
-    for i in range(1, n):    # O(n)
-        indexes = range(i, n) if i % 2 == 0 else range(n - 1, i - 1, -1)    # O(n)
-        result.extend(matrix[j][i + n - j - 1] for j in indexes)    # O(n)
-
-    print(", ".join(map(str, result)))    # O(1)
+MergeSort(a,aux,0,len(a)-1)
+print(aux)
 
 
-def main():    # O(n)
-    n = int(input("Ingrese el valor de n para la matriz nxn: "))    # O(1)
-    matrix = [[i * n + j + 1 for j in range(n)] for i in range(n)]        # O(n)
-    print("Matriz generada:")        # O(1)
-    for row in matrix:    # O(n)
-        print(row)    # O(n)
-    print("\nElementos en diagonal zigzag:")    # O(1)
-    if n % 2 == 0:    # O(1)
-      print_diagonal_zigzag(matrix)    # O(1)
-    else:    # O(1)
-      print_diagonal_zigzag_IMP(matrix)    # O(1)
-
-if __name__ == "__main__":        # O(1)
-    main()# O(1)
